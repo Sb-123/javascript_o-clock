a5d97ae97485512e4f67675be391a60468c4101a
Sanjeev Yadav <85263808+Sb-123@users.noreply.github.com>
2024-04-24 12:10:53 +0000

    arrow function last

--- 3. Introduction/03_arrow_function.js
+++ 3. Introduction/03_arrow_function.js
@@ -14,11 +14,43 @@ const user={
 // user.welcomeMessage()
 
 
-console.log(this);
+// console.log(this); // o/p:- {} means that "this" gives the empty object without putting at anything i.e object and function.
+
 // ************************************************  "this" inside the function    ************************************************************************************************
-function chai(){
+// function chai(){
+//     let username="Sanjeev"
+//     console.log(this.username);   // o/p:-undefined b/c we can't use "this" inside the function but, we can use it inside the object.
+//     // console.log(this);
+// }
+// chai();
+// ****************************************** arrow function ****************************************************
+const chai= () =>{
     let username="Sanjeev"
-    console.log(this.username);   // o/p:-undefined b/c we can use "this" inside the function but, we can use it inside the object.
+    console.log(this); 
+
 }
-chai();
-// ******************************************   ****************************************************
+chai();// o/p:-{}, it fetch from arrow function.
+
+// ************************************** pure arrow function ***********************
+
+
+// const addTwo=(num1,num2)=>{
+//     return num1+num2;
+// }
+// console.log(addTwo(3,4)); // o/p:-7.
+
+// ************************************************** implicit return ***************************************
+
+// const addTwo=(num1,num2)=>  (num1+num2); // use parenthesis and not use the return keyword.
+
+
+// const addTwo = (num1, num2)=>(num1+num2)
+const addTwo=(num1,num2)=>({username:"Saneev"})
+
+
+console.log(addTwo(7,8)); // o/p:-15.
+
+// *********************************************  moreover  ************************************
+
+// this  is the globel Object. 
+
---
+++ 3. Introduction/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+const chai=function(){
+//     let username="Sanjeev"
+//     console.log();
+// }
