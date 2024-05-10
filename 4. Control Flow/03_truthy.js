ad4f2e19d460ad664e8fb5008853ef72c01a8837
Sanjeev Yadav <85263808+Sb-123@users.noreply.github.com>
2024-05-10 18:47:07 +0000

    terniary operator

--- 4. Control Flow/03_truthy.js
+++ 4. Control Flow/03_truthy.js
@@ -24,3 +24,29 @@ if(Object.keys(emptyObj).length===0){
     console.log("Object is empty");
    
 }
+
+
+// ************************************** nullish Coalescing Operator (??): null undefined  **********************************************
+
+let val1;
+val1=5 ?? 10;
+console.log(val1);  //o/p:-5
+val2= null ?? 10;
+console.log(val2);  // o/p:-10
+
+var3=undefined ?? 15
+console.log(var3);  // o/p:-15
+
+val4= null ?? 10 ?? 15;
+console.log(val4);  // o/p:-10
+
+// *********************************** Terniary Operator  *************************************************************************
+
+// condition ? true :false;
+const iceTeaPrice=100
+iceTeaPrice<=80 ? console.log("less than 80") : console.log("more than 80");  // o/p:- more than 80.
+
+// *********************************************************************************************************************************
+
+
+
