c4d3956ae960bc7c2f6aeb2c925f8235c2b5f54b
Sanjeev Yadav <85263808+Sb-123@users.noreply.github.com>
2024-05-10 18:13:44 +0000

    just again started after 7th seme

--- 4. Control Flow/01_one.js
+++ 4. Control Flow/01_one.js
@@ -43,11 +43,15 @@ const balance=10000;
 
 
 
-
 const userLoggedIn=true;
 const debitCard=true;
+const loggedInFromEmail= true;
 
 if(userLoggedIn && debitCard && 2==2){
     console.log("Allow to buy the course");
 
+}
+
+if(loggedInFromGoogle || loggedInFromEmail){
+    console.log("User logged in");
 }
