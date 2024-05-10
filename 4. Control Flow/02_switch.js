617fa16c2532150b964bbe53792aa56875d54c97
Sanjeev Yadav <85263808+Sb-123@users.noreply.github.com>
2024-05-10 18:28:38 +0000

    switch and truthy

---
+++ 4. Control Flow/02_switch.js
@@ -0,0 +1,33 @@
+
+//
+
+
+const month= 3
+
+
+switch(month){
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    case 5:
+        console.log("may");
+        break;
+    case 6:
+        console.log("june");
+        break;
+    case 7:
+        console.log("july");
+        break;
+
+    default:
+        break;
+}
