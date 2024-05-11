9577d21dba47a9d7c7ccf1d6d4adb96c72b2c47b
Sanjeev Yadav <85263808+Sb-123@users.noreply.github.com>
2024-05-11 03:20:57 +0000

    iteration

---
+++ 5. iteration/02_while_loop.js
@@ -0,0 +1,53 @@
+// ******************************************************  while loop concepts  ***************************************************************************
+let index=0;
+while (index<=10) {
+    console.log(`Value of index is ${index}`);
+    index=index+2;
+    
+}
+/* o/p:-
+Value of index is 0
+Value of index is 2
+Value of index is 4
+Value of index is 6
+Value of index is 8
+Value of index is 10
+*/
+
+// *******************************************************************************************************************************************************
+
+
+
+let myArray=["flash","batman","superman"]
+let arr=0;
+while (arr<myArray.length) {
+    console.log(`Value is ${myArray[arr]}`);
+    arr++;
+    
+}
+
+// ************************************************************** do while loop concepts   *****************************************************************************************
+let score =1;
+do{
+    console.log(`Score is ${score}`);
+    score++;
+}while(score<=10)
+
+
+/*o/p:-
+Score is 1
+Score is 2
+Score is 3
+Score is 4
+Score is 5
+Score is 6
+Score is 7
+Score is 8
+Score is 9
+Score is 10
+
+*/
+
+// *******************************************************************************************************************************************************
+
+
