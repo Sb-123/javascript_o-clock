2192966794393eb9a6c043d5ca3a4a6912f0d8ac
Sanjeev Yadav <85263808+Sb-123@users.noreply.github.com>
2024-05-11 04:27:11 +0000

    for of loop

--- 5. iteration/02_while_loop.js
+++ 5. iteration/02_while_loop.js
@@ -48,6 +48,91 @@ Score is 10
 
 */
 
-// *******************************************************************************************************************************************************
+// ***************************************************************** for of loops (array specific loop) **************************************************************************************
 
+
+// ["","",""]
+// [{},{},{}]
+
+const arr2=[1,2,3,4,5]
+for(const num of arr2){
+    console.log(num);
+
+}
+/*o/p:-
+1
+2
+3
+4
+5
+*/
+
+const greetings="hello world!"
+for(const greet of greetings){
+    console.log(`Each char is ${greet}`);
+}
+/*o/p:-
+Each char is h
+Each char is e
+Each char is l
+Each char is l
+Each char is o
+Each char is  
+Each char is w
+Each char is o
+Each char is r
+Each char is l
+Each char is d
+Each char is !
+
+*/
+
+// ******************************************************************** maps (it's a type of datatype) ***********************************************************************************
+
+const map =new Map();
+map.set("IN", "India")
+map.set("USA","United State of America")
+map.set("Fr","France")
+map.set("IN","India")
+// maps takes only unique value.
+// aur jis order mai enter kra hai usi order mai rhaa krta hai maps.
+
+console.log(map);
+/*o/p:-
+  'IN' => 'India',
+  'USA' => 'United State of America',
+  'Fr' => 'France'
+ */
+
+for(const key of map){
+    console.log(key);
+}
+/*o/p:-
+[ 'IN', 'India' ]
+[ 'USA', 'United State of America' ]
+[ 'Fr', 'France' ]
+
+*/
+for(const [key,value] of map){
+    console.log(key, ':-', value);
+}
+/*o/p:-
+IN :- India
+USA :- United State of America
+Fr :- France
+ */
+
+// ************************************************************ 'for of' loops in objects *******************************************************************************************
+
+
+const myObject={
+    'game1':'NFS',
+    'game2':'spiderman'
+}
+
+for(const [key,value] of myObject){
+    console.log(key,':-',value);  // o/p:-error b/c objects are not be iteratable like as map.
+}
+
+// *******************************************************************************************************************************************************
 
---
+++ 5. iteration/03_for_of_loops.js
@@ -0,0 +1,54 @@
+const myObject={
+    js:'javascript',
+    cpp: 'c++',
+    rb:"ruby",
+    swift:"swift by apple"
+
+}
+
+for(const key in myObject){
+    console.log(key);
+}
+/*o/p:-
+js
+cpp
+rb
+swift
+
+ */
+for(const key in myObject){
+    console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+/*o/p:-
+js shortcut is for javascript
+cpp shortcut is for c++
+rb shortcut is for ruby
+swift shortcut is for swift by apple
+ */
+
+// ****************************************************************** array has also key which is number which is started from 0 *************************************************************************************
+const programming=["js","rb","py","java","cpp"]
+
+for(const key in programming){
+    console.log(key);
+}
+/*o/p:-
+0
+1
+2
+3
+4
+ */
+for(const key in programming){
+    console.log(programming[key]);
+}
+/*o/p:-
+js
+rb
+py
+java
+cpp
+ */
+// *******************************************************************************************************************************************************
+
+
