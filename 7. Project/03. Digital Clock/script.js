0cc4b942465c61f5d3e7cd140ca8431be1c0386f
Sanjeev Yadav <85263808+Sb-123@users.noreply.github.com>
2024-05-31 18:40:45 +0000

    guess the number

---
+++ 7. Project/03. Digital Clock/script.js
@@ -0,0 +1,16 @@
+ const clock=document.getElementById('clock')
+// document.querySelector('#clock')
+
+// let date= new Date()
+// console.log(date.toLocaleTimeString());
+
+
+setInterval(function(){
+  let date= new Date()
+// console.log(date.toLocaleTimeString());
+clock.innerHTML=date.toLocaleTimeString();
+
+
+},1000);
+
+// setInterval(function(){},50000);
