9589416dfd13e8d6066f70151be29ac621d10e36
Sanjeev Yadav <85263808+Sb-123@users.noreply.github.com>
2024-07-02 03:41:03 +0000

    background color changer by Hitesh chu

---
+++ 7. Project/06. background color/scripts.js
@@ -0,0 +1,41 @@
+/* generate a random color */
+
+const randomColor= function(){
+    const hex="0123456789ABCDEF"
+
+    let color='#'
+    for(let i=0;i<6;i++){
+        color +=hex[ Math.floor(Math.random()*16)];
+        
+
+    }
+    return color;
+}
+
+/* console.log(randomColor()); */
+
+
+
+let interValId;
+const startChangingColor=function(){
+    if(!interValId){
+    interValId = setInterval(changeBgColor,5000);
+
+    }
+    function changeBgColor(){
+
+        document.body.style.backgroundColor=randomColor();
+    }
+
+};
+
+const stopChangingColor=function(){
+    clearInterval(interValId)
+    interValId=null;
+
+}
+document.querySelector("#start").addEventListener('click', startChangingColor);
+
+document.querySelector("#stop").addEventListener('click',stopChangingColor);
+
+// console.log("Sanjeev");
