8dabe2d5c2f10afcd2c10a5112f8a81fce89e2dc
Sanjeev Yadav <85263808+Sb-123@users.noreply.github.com>
2024-08-14 08:48:53 +0530

    Update 06_numsAndMaths.js

--- 1. Introduction/06_numsAndMaths.js
+++ 1. Introduction/06_numsAndMaths.js
@@ -1,16 +1,30 @@
 const score=400;
 const balance= new Number(100)
 console.log(balance);
+/* o/p:- 
+[Number:100]
+*/
 
 console.log(balance.toString());
 console.log(balance.toString().length);
 console.log(balance.toFixed(1));
+/*o/p:-
+100
+3
+100.0
+*/
 
 const otherNumber=23.8995;
 console.log(otherNumber.toPrecision(3));
+/*o/p:-
+23.9
+*/
 
 const hundreds =100000;
 console.log(hundreds.toLocaleString('en-IN'));
+/* o/p:-
+10,00,000
+*/
 
 
 
@@ -25,6 +39,17 @@ console.log(Math.floor(9.2));
 console.log(Math.min(9.2));
 console.log(Math.max(9.2));
 console.log(Math.floor(Math.random(9.2)));
+/*
+o/p:-
+Object [Math] {}
+4
+5
+5
+9
+9.2
+9.2
+0
+*/
 
 
 const min=10;
